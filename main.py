3989cc76acdaf5b40e5e90c99a9e34217936cdc9
William Melo <william.quintas@gmail.com>
2020-07-27 00:07:21 -0300

    change Field(), initialization() and onDisplay()
    defines getters and setters for Field attributes
    changes initialization variables declaration
    creates onTimer functions
    comments variables meaning on initialization()
    creates onDisplay() function

--- main.py
+++ main.py
@@ -11,52 +11,104 @@ class Field():
         self.carPosition = None
         self.treeHeight = None
 
+    def getForestOrStreet(self):
+        return self.forestOrStreet
+
+    def getEmpty(self):
+        return self.empty
+
+    def getCarPosition(self):
+        return self.carPosition
+
+    def getTreeHeight(self):
+        return self.treeHeight
+
+    def setForestOrStreet(self, value):
+        self.forestOrStreet = value
+
+    def setEmpty(self, value):
+        self.empty = value
+
+    def setCarPosition(self, value):
+        self.carPosition = value
+
+    def setTreeHeight(self, value):
+        self.treeHeight = value
+
 
 # initializes fields matrix
 fieldsSize = 20
 fieldsMatrix = np.full((fieldsSize, fieldsSize), Field())
 
 
-def fieldsInitialization():
-    global fieldsInitialized
-    fieldsInitialized = 0
-    global zTrackBegin
-    zTrackBegin = -12
-    global alpha
-    alpha = 0
-    global beginAnimation
-    beginAnimation = 1
+def initialization():
+    # declare variables
+    global alpha  # player rotation angle
+    global beginAnimation  # animation when player jumps control
+    global carHitPlayer  # 1 if any car hit the player
+    global crashedInSomething  # 1 if the player crashed in something
+    global fieldsInitialized  # variable to control the fields are once initialized
+    global jump  # stores pressed key to control player direction
+    global previousJump  # stores previous pressed key to control player rotation
     global runTime1
-    runTime1 = 0
     global runTime2
-    runTime2 = 0
-    global runTime3
-    runTime3 = 0
-    global jump
-    jump = 'w'
-    global previousJump
-    previousJump = 'w'
-    global xCurr
-    xCurr = 0
-    global yCurr
-    yCurr = 0.5
-    global zCurr
-    zCurr = 0
+    global t  # time variable for cars movement
+    global xCurrent  # player position
+    global xPrevious
+    global yCurrent
+    global yPrevious
+    global zCurrent
+    global zPrevious
     global zTime
-    zTime = 0
-    global crashedInSomething
-    crashedInSomething = 0
-    global carHitPlayer
+    global zTrackBegin  # terrain starts in z=-12
+
+    # initialize variables
+    alpha = 0
+    beginAnimation = 1
     carHitPlayer = 0
-    global reverse
-    reverse = 0
-    global t
+    crashedInSomething = 0
+    fieldsInitialized = 0
+    jump = 'w'
+    previousJump = 'w'
+    runTime1 = 0
+    runTime2 = 0
     t = 0
+    xCurr = 0
+    yCurr = 0.5
+    zCurr = 0
+    zTime = 0
+    zTrackBegin = -12
+
+
+# car start timer
+def onTimer1(value: int):
+    return None
+# player jump timer
+
+
+def onTimer2(value: int):
+    return None
 
 
 def onDisplay():
+    # delete previous screen content
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    # configure camera
+    glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
+    gluLookAt(1, 7 - yCurr, 3, 0, 0 - yCurr, -2, 0, 1, 0)
+    if (fieldsInitialized == 0):
+        zTrackBegin = -12 - 3
+        fieldsInitialization()
+        fieldsInitialized = 1
+        fieldsMatrix[10][-zTrackBegin].setEmpty(1)
+
+    # configureIllumination()
+    # renderTerrain()
+    # defineTreesPosition()
+    # renderStreets()
+    # renderPlayer()
+
     glutSwapBuffers()
 
 
@@ -81,7 +133,7 @@ def main():
     glutKeyboardFunc(onKeyboard)
     glutReshapeFunc(onReshape)
     # initializes game controls
-    fieldsInitialization()
+    initialization()
     # Initializes OpenGL
     glClearColor(1, 1, 1, 0)
     glEnable(GL_DEPTH_TEST)
