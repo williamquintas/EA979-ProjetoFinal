586e1baa9a3641b3e6823e27e345b1355a6abda1
William Melo <william.quintas@gmail.com>
2020-07-26 23:22:05 -0300

    create Field class and global variables for the game

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
+import numpy as np
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+
+
+class Field():
+    def __init__(self):
+        self.forestOrStreet = None
+        self.empty = None
+        self.carPosition = None
+        self.treeHeight = None
+
+
+# initializes fields matrix
+fieldsSize = 20
+fieldsMatrix = np.full((fieldsSize, fieldsSize), Field())
+
+
+def fieldsInitialization():
+    global fieldsInitialized
+    fieldsInitialized = 0
+    global zTrackBegin
+    zTrackBegin = -12
+    global alpha
+    alpha = 0
+    global beginAnimation
+    beginAnimation = 1
+    global runTime1
+    runTime1 = 0
+    global runTime2
+    runTime2 = 0
+    global runTime3
+    runTime3 = 0
+    global jump
+    jump = 'w'
+    global previousJump
+    previousJump = 'w'
+    global xCurr
+    xCurr = 0
+    global yCurr
+    yCurr = 0.5
+    global zCurr
+    zCurr = 0
+    global zTime
+    zTime = 0
+    global crashedInSomething
+    crashedInSomething = 0
+    global carHitPlayer
+    carHitPlayer = 0
+    global reverse
+    reverse = 0
+    global t
+    t = 0
 
 
 def onDisplay():
@@ -9,7 +60,7 @@ def onDisplay():
     glutSwapBuffers()
 
 
-def onKeyboard():
+def onKeyboard(key: str, x: int, y: int):
     return null
 
 
@@ -30,7 +81,7 @@ def main():
     glutKeyboardFunc(onKeyboard)
     glutReshapeFunc(onReshape)
     # initializes game controls
-    # gameInitialization()
+    fieldsInitialization()
     # Initializes OpenGL
     glClearColor(1, 1, 1, 0)
     glEnable(GL_DEPTH_TEST)
