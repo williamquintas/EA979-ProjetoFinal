7629c098c762017efd2e679c061a611fda7ad753
William Melo <william.quintas@gmail.com>
2020-07-28 22:23:34 -0300

    remove getters and setters

--- main.py
+++ main.py
@@ -15,30 +15,6 @@ class Field():
         self.carPosition = None
         self.treeHeight = None
 
-    def getForestOrStreet(self):
-        return self.forestOrStreet
-
-    def getEmpty(self):
-        return self.empty
-
-    def getCarPosition(self):
-        return self.carPosition
-
-    def getTreeHeight(self):
-        return self.treeHeight
-
-    def setForestOrStreet(self, value):
-        self.forestOrStreet = value
-
-    def setEmpty(self, value):
-        self.empty = value
-
-    def setCarPosition(self, value):
-        self.carPosition = value
-
-    def setTreeHeight(self, value):
-        self.treeHeight = value
-
 
 # initializes fields matrix
 fieldsSize = 20
@@ -155,7 +131,7 @@ def onDisplay():
         zTrackBegin = -12 - 3
         fieldsInitialization()
         fieldsInitialized = 1
-        fieldsMatrix[10][-zTrackBegin].setEmpty(1)
+        fieldsMatrix[10][-zTrackBegin].empty = 1
 
     configureIllumination()
     renderForest()
@@ -192,7 +168,7 @@ def onKeyboard(key: str, x: int, y: int):
     if keycode == 27:
         sys.exit()
     elif key == 'w':  # moves forward
-        if (fieldsMatrix[xCurrent + 10][-zTrackBegin - 1].getEmpty() == 1) and beginAnimation == 1 and runTime2 == 0:
+        if (fieldsMatrix[xCurrent + 10][-zTrackBegin - 1].empty == 1) and beginAnimation == 1 and runTime2 == 0:
             alpha = 0
             jump = 'w'
             runTime2 = 1
@@ -208,7 +184,7 @@ def onKeyboard(key: str, x: int, y: int):
             glutPostRedisplay()
 
     elif key == 'a':  # moves to left
-        if fieldsMatrix[xCurrent + 9][-zTrackBegin].getEmpty() == 1 and beginAnimation == 1 and runTime2 == 0:
+        if fieldsMatrix[xCurrent + 9][-zTrackBegin].empty == 1 and beginAnimation == 1 and runTime2 == 0:
             alpha = 0
             jump = 'a'
             runTime2 = 1
@@ -219,7 +195,7 @@ def onKeyboard(key: str, x: int, y: int):
                 glutTimerFunc(10, onTimer1, 0)
 
     elif key == 'd':  # moves to right
-        if fieldsMatrix[xCurrent + 11][-zTrackBegin].getEmpty() == 1 and beginAnimation == 1 and runTime2 == 0:
+        if fieldsMatrix[xCurrent + 11][-zTrackBegin].empty == 1 and beginAnimation == 1 and runTime2 == 0:
             alpha = 0
             jump = 'd'
             runTime2 = 1
@@ -230,7 +206,7 @@ def onKeyboard(key: str, x: int, y: int):
                 glutTimerFunc(10, onTimer1, 0)
 
     elif key == 's':  # moves back
-        if fieldsMatrix[xCurrent + 10][-zTrackBegin + 1].getEmpty() == 1 and beginAnimation == 1 and runTime2 == 0:
+        if fieldsMatrix[xCurrent + 10][-zTrackBegin + 1].empty == 1 and beginAnimation == 1 and runTime2 == 0:
             alpha = 0
             jump = 's'
             runTime2 = 1
@@ -268,31 +244,31 @@ def moveObjects():
     if jump == 'w':
         for j in range(18, 0, -1):
             for i in range(0, 20):
-                fieldsMatrix[i][j+1].setEmpty(fieldsMatrix[i][j].getEmpty())
-                fieldsMatrix[i][j+1].setForestOrStreet(fieldsMatrix[i][j].getForestOrStreet())
-                fieldsMatrix[i][j+1].setCarPosition(fieldsMatrix[i][j].getCarPosition())
-                fieldsMatrix[i][j + 1].setTreeHeight(fieldsMatrix[i][j].getTreeHeight())
+                fieldsMatrix[i][j+1].empty = fieldsMatrix[i][j].empty
+                fieldsMatrix[i][j+1].forestOrStreet = fieldsMatrix[i][j].forestOrStreet
+                fieldsMatrix[i][j+1].carPosition = fieldsMatrix[i][j].carPosition
+                fieldsMatrix[i][j + 1].treeHeight = fieldsMatrix[i][j].treeHeight
         for j in range(0, 20):
-            fieldsMatrix[j][0].setEmpty(1)
-            if fieldsMatrix[j][1].getForestOrStreet() == 'street' and fieldsMatrix[j][2].getForestOrStreet() == 'street':
-                fieldsMatrix[j][0].getForestOrStreet() == 'forest'
+            fieldsMatrix[j][0].empty = 1
+            if fieldsMatrix[j][1].forestOrStreet == 'street' and fieldsMatrix[j][2].forestOrStreet == 'street':
+                fieldsMatrix[j][0].forestOrStreet == 'forest'
             else:
-                fieldsMatrix[j][0].getForestOrStreet() == 'street'
+                fieldsMatrix[j][0].forestOrStreet == 'street'
         for i in range(0, 20):
-            if rd.randint(0, maxSize) / maxSize > 0.7 and fieldsMatrix[i][0].getForestOrStreet() == 'forest':
-                fieldsMatrix[i][0].setTreeHeight(mt.ceil(rd.randint(0, maxSize) / maxSize * 3))
-                fieldsMatrix[i][0].setEmpty(0)
+            if rd.randint(0, maxSize) / maxSize > 0.7 and fieldsMatrix[i][0].forestOrStreet == 'forest':
+                fieldsMatrix[i][0].treeHeight = mt.ceil(rd.randint(0, maxSize) / maxSize * 3)
+                fieldsMatrix[i][0].empty = 0
             else:
-                fieldsMatrix[i][0].setEmpty(1)
-            fieldsMatrix[i][0].setCarPosition(rd.randint(0, maxSize) / maxSize * 8 + 10 * i + 10 * t)
+                fieldsMatrix[i][0].empty = 1
+            fieldsMatrix[i][0].carPosition = rd.randint(0, maxSize) / maxSize * 8 + 10 * i + 10 * t
     # if the player moves backward, all the objects are moved forward in one field
     elif jump == 's':
         for j in range(0, 20):
             for i in range(0, 20):
-                fieldsMatrix[i][j - 1].setEmpty(fieldsMatrix[i][j].getEmpty())
-                fieldsMatrix[i][j - 1].setForestOrStreet(fieldsMatrix[i][j].getForestOrStreet())
-                fieldsMatrix[i][j - 1].setCarPosition(fieldsMatrix[i][j].getCarPosition())
-                fieldsMatrix[i][j - 1].setTreeHeight(fieldsMatrix[i][j].getTreeHeight())
+                fieldsMatrix[i][j - 1].empty = fieldsMatrix[i][j].empty
+                fieldsMatrix[i][j - 1].forestOrStreet = fieldsMatrix[i][j].forestOrStreet
+                fieldsMatrix[i][j - 1].carPosition = fieldsMatrix[i][j].carPosition
+                fieldsMatrix[i][j - 1].treeHeight = fieldsMatrix[i][j].treeHeight
 
 
 # random tree size and car position settings
@@ -304,19 +280,19 @@ def fieldsInitialization():
     for x in range(0, 20):
         for y in range(0, 20):
             if (y > 5):
-                fieldsMatrix[x][y].setForestOrStreet('forest')
+                fieldsMatrix[x][y].forestOrStreet = 'forest'
             elif (y % 3 == 0):
-                fieldsMatrix[x][y].setForestOrStreet('forest')
+                fieldsMatrix[x][y].forestOrStreet = 'forest'
             else:
-                fieldsMatrix[x][y].setForestOrStreet('street')
+                fieldsMatrix[x][y].forestOrStreet = 'street'
 
-            if (rd.randint(0, maxSize) / maxSize > 0.9 and fieldsMatrix[x][y].getForestOrStreet() == 'forest'):
-                fieldsMatrix[x][y].setEmpty(0)
-                fieldsMatrix[x][y].setTreeHeight(mt.ceil(rd.randint(0, maxSize) / maxSize * 3))
+            if (rd.randint(0, maxSize) / maxSize > 0.9 and fieldsMatrix[x][y].forestOrStreet == 'forest'):
+                fieldsMatrix[x][y].empty = 0
+                fieldsMatrix[x][y].treeHeight = mt.ceil(rd.randint(0, maxSize) / maxSize * 3)
             else:
-                fieldsMatrix[x][y].setEmpty(1)
+                fieldsMatrix[x][y].empty = 1
 
-            fieldsMatrix[x][y].setCarPosition(rd.randint(0, maxSize) / maxSize * 8 + 10 * x)
+            fieldsMatrix[x][y].carPosition = rd.randint(0, maxSize) / maxSize * 8 + 10 * x
 
 
 def configureIllumination():
@@ -343,13 +319,13 @@ def renderForest():
     glTranslatef(-xCurrent, -yCurrent, -zCurrent)
     for i in range(0, 20):
         for j in range(0, 20):
-            if fieldsMatrix[i][j].getEmpty() == 0 and fieldsMatrix[i][j].getForestOrStreet() == 'forest':
+            if fieldsMatrix[i][j].empty == 0 and fieldsMatrix[i][j].forestOrStreet == 'forest':
                 renderTree(i - 10, zTrackBegin + j)
     for j in range(0, 20):
-        if fieldsMatrix[0+1][j].getForestOrStreet() == 'forest':
+        if fieldsMatrix[0+1][j].forestOrStreet == 'forest':
             renderTree(0 - 10 - 1, zTrackBegin + j)
     for j in range(0, 20):
-        if fieldsMatrix[20-1][j].getForestOrStreet() == 'forest':
+        if fieldsMatrix[20-1][j].forestOrStreet == 'forest':
             renderTree(20-10, zTrackBegin + j)
     glPopMatrix()
 
@@ -369,7 +345,7 @@ def renderTree(x: int, z: int):
     glScalef(0.8, 0.8, 0.8)
 
     if (x > -11 and x < 10):
-        aux = fieldsMatrix[x + 10][z - zTrackBegin].getTreeHeight()
+        aux = fieldsMatrix[x + 10][z - zTrackBegin].treeHeight
     else:
         aux = 3
 
@@ -416,17 +392,17 @@ def renderTerrain():
     glTranslatef(-xCurrent, -yCurrent, -zCurrent)
     for i in range(0, 20):
         for j in range(0, 20):
-            if fieldsMatrix[i][j].getForestOrStreet() != 'street':
+            if fieldsMatrix[i][j].forestOrStreet != 'street':
                 renderGrass(i, j)
 
     for i in range(-10, 0):
         for j in range(0, 20):
-            if fieldsMatrix[i + 15][j].getForestOrStreet() != 'street':
+            if fieldsMatrix[i + 15][j].forestOrStreet != 'street':
                 renderGrass(i, j)
 
     for i in range(20, 30):
         for j in range(0, 20):
-            if fieldsMatrix[i - 15][j].getForestOrStreet() != 'street':
+            if fieldsMatrix[i - 15][j].forestOrStreet != 'street':
                 renderGrass(i, j)
     glPopMatrix()
 
@@ -458,20 +434,20 @@ def renderStreets():
     glTranslatef(-xCurrent, -yCurrent, -zCurrent)
     for i in range(0, 20):
         for j in range(0, 20):
-            if fieldsMatrix[i][j].getForestOrStreet() == 'street':
+            if fieldsMatrix[i][j].forestOrStreet == 'street':
                 renderAsphalt(i, j)
     for i in range(-10, 0):
         for j in range(0, 20):
-            if fieldsMatrix[i+15][j].getForestOrStreet() == 'street':
+            if fieldsMatrix[i+15][j].forestOrStreet == 'street':
                 renderAsphalt(i, j)
     for i in range(20, 30):
         for j in range(0, 20):
-            if fieldsMatrix[i-15][j].getForestOrStreet() == 'street':
+            if fieldsMatrix[i-15][j].forestOrStreet == 'street':
                 renderAsphalt(i, j)
     for i in range(0, 20):
         for j in range(0, 20):
-            if fieldsMatrix[i][j].getForestOrStreet() == 'street':
-                renderCar(fieldsMatrix[i][j].getCarPosition(), j)
+            if fieldsMatrix[i][j].forestOrStreet == 'street':
+                renderCar(fieldsMatrix[i][j].carPosition, j)
     glPopMatrix()
 
 
@@ -484,15 +460,15 @@ def renderAsphalt(x, z):
         (
             x >= 0 and
             x < 20 and
-            fieldsMatrix[x][z + 1].getForestOrStreet() == 'street' and
+            fieldsMatrix[x][z + 1].forestOrStreet == 'street' and
             x % 2 == 1
         ) or (
             x < 0 and
-            fieldsMatrix[x + 15][z + 1].getForestOrStreet() == 'street' and
+            fieldsMatrix[x + 15][z + 1].forestOrStreet == 'street' and
             np.abs(x) % 2 == 1
         ) or (
             x >= 20 and
-            fieldsMatrix[x - 15][z + 1].getForestOrStreet() == 'street' and
+            fieldsMatrix[x - 15][z + 1].forestOrStreet == 'street' and
             x % 2 == 1
         )
     ):
@@ -671,23 +647,23 @@ def renderCar(x, z):
 
     position = np.ceil(t * 10 - x)
     if (position == 0 or position == 1):
-        fieldsMatrix[position][z].setEmpty(0)
+        fieldsMatrix[position][z].empty = 0
 
     if (position > 1 and position < 19):
-        fieldsMatrix[position][z].setEmpty(0)
-        fieldsMatrix[position - 1][z].setEmpty(0)
-        fieldsMatrix[position - 2][z].setEmpty(1)
-        fieldsMatrix[position + 1][z].setEmpty(1)
+        fieldsMatrix[position][z].empty = 0
+        fieldsMatrix[position - 1][z].empty = 0
+        fieldsMatrix[position - 2][z].empty = 1
+        fieldsMatrix[position + 1][z].empty = 1
 
     if (position == 19):
-        fieldsMatrix[position][z].setEmpty(0)
-        fieldsMatrix[position - 2][z].setEmpty(1)
+        fieldsMatrix[position][z].empty = 0
+        fieldsMatrix[position - 2][z].empty = 1
 
     if (position == 20):
-        fieldsMatrix[position - 2][z].setEmpty(1)
+        fieldsMatrix[position - 2][z].empty = 1
 
     if (position == 21):
-        fieldsMatrix[position - 2][z].setEmpty(1)
+        fieldsMatrix[position - 2][z].empty = 1
 
     if (position - 10 == x_curr and z + zTrackBegin == 0):
         carHitPlayer = 1
@@ -702,7 +678,7 @@ def renderPlayer():
     global previousJump
     global runTime1
     pi = 3.1415
-    
+
     glPushMatrix()
 
     if (crashedInSomething == 1):
@@ -711,7 +687,7 @@ def renderPlayer():
         glScalef(1, 1, 0.2)
     if (previousJump == 'a'):
         glRotatef(70, 0, 1, 0)
-    
+
     if (previousJump == 'd'):
         glRotatef(-70, 0, 1, 0)
 
@@ -723,49 +699,49 @@ def renderPlayer():
 
     if (jump == 'a' and previousJump == 'a'):
         glRotatef(-90, 0, 1, 0)
-        
+
     elif (jump == 'a' and previousJump == 'd'):
         glRotatef(90 + alpha * 180 / pi, 0, 1, 0)
-        
+
     elif (jump == 'a' and previousJump == 'w'):
         glRotatef(180 + alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 'a' and previousJump == 's'):
         glRotatef(0 - alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 'd' and previousJump == 'd'):
         glRotatef(90, 0, 1, 0)
-        
+
     elif (jump == 'd' and previousJump == 'w'):
         glRotatef(180 - alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 'd' and previousJump == 'a'):
         glRotatef(-90 - alpha * 180 / pi, 0, 1, 0)
-        
+
     elif (jump == 'd' and previousJump == 's'):
         glRotatef(0 + alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 'w' and previousJump == 'd'):
         glRotatef(90 + alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 'w' and previousJump == 'w'):
         glRotatef(180, 0, 1, 0)
-        
+
     elif (jump == 'w' and previousJump == 's'):
         glRotatef(0 - alpha * 180 / pi, 0, 1, 0)
-        
+
     elif (jump == 'w' and previousJump == 'a'):
         glRotatef(-90 - alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 's' and previousJump == 'a'):
         glRotatef(-90 + alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 's' and previousJump == 'd'):
         glRotatef(90 - alpha * 90 / pi, 0, 1, 0)
-        
+
     elif (jump == 's' and previousJump == 's'):
         glRotatef(0, 0, 1, 0)
-        
+
     elif (jump == 's' and previousJump == 'w'):
         glRotatef(180 + alpha * 180 / pi, 0, 1, 0)
 
